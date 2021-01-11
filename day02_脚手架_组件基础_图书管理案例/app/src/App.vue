9b1c5b4f22d930cfdae1a655ad25b4812605bccd
wbc <417780894@qq.com>
2021-01-11 11:04:21 +0800

    cli

---
+++ day02_脚手架_组件基础_图书管理案例/app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="app">
+        <div>{{msg}}</div>
+        <button @click="handler(111, $event)">点我</button>
+	</div>
+</template>
+
+<script>
+export default {
+    // data:{}
+    data() { // 组件中data必须是一个函数 return一个对象
+        return {
+            msg: '我是数据'
+        }
+    },
+    methods: {
+        handler(a, e) {
+            // alert(123)
+            console.log(a)
+            console.log(e)
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+    button {
+        color: red;
+    }
+</style>
