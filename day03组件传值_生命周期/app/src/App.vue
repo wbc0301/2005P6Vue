e972b5e251fe416786a2a333c3200f44617c367e
wbc <417780894@qq.com>
2021-01-12 10:15:23 +0800

    父子传值

--- day03组件传值_生命周期/app/src/App.vue
+++ day03组件传值_生命周期/app/src/App.vue
@@ -1,12 +1,19 @@
 <template>
 	<div id="app">
         <h1>我是父组件</h1>
+        <div class="abc" aa="123">{{str}}{{present}}</div>
+        <div>儿子要送给我一个礼物：{{pre}}</div>
 		<!-- <p>
             <a href="http://www.baidu.com">百度</a>
 		</p>-->
 
         <!-- 3: 使用组件 -->
-        <one></one>
+        <!-- 通过自定属性的形式往子组件传递数据 -->
+        <!-- <one a="1" b="2" c="3" ></one> -->
+        
+        <!-- 通过注册自定义事件绑定父组件的一个函数，在子组件触发这个自定义事件时，调用这个函数 -->
+        <!-- 函数是父组件的，也就是说，改变数据的逻辑是在父组件中处理的 -->
+        <one :present="present" @aa="handler"></one>
         
 	</div>
 </template>
@@ -22,14 +29,21 @@ export default {
     name: 'App',// 组件的名字
 	data() {
 		return {
-
+            str: '我是父组件的数据',
+            present: '玩具车',
+            pre: '',
 		}
 	},
 	components: { // 这里是注册子组件的地方
         // 2：注册组件
         // one: one  
         one // es6对象的简写写法
-	}
+    },
+    methods: {
+        handler(val) {
+            this.pre = val;
+        }
+    }
 
 }
 </script>
@@ -38,5 +52,6 @@ export default {
 #app {
     height: 500px;
     background-color: pink;
+    padding: 20px;
 }
 </style>
--- day03组件传值_生命周期/app/src/components/one.vue
+++ day03组件传值_生命周期/app/src/components/one.vue
@@ -1,16 +1,30 @@
 <template>
 	<div class="one">
 		<h3>我是孩子组件</h3>
+        <div>{{str}}</div>
+        <div>爸爸送了我一个：{{present}}</div>
+        <button @click="song">按钮</button>
+        
 	</div>
 </template>
 
 <script>
 export default {
     name: 'one', // 组件的名字
+    // props: ['a','b'], // 接收父组件传递过来的数据
+    props: ['present'], // 接收父组件传递过来的数据
 	data() {
-		return {};
+		return {
+            str: '我是子组件的数据',
+            pr: '老是让你去学校一趟'
+        };
 	},
-	methods: {},
+	methods: {
+        song() {
+            // 触发自定义事件 往父组件传值
+            this.$emit('aa', this.pr)
+        }
+    },
 };
 </script>
 
@@ -18,5 +32,7 @@ export default {
 .one {
 	height: 200px;
 	background-color: skyblue;
+    padding: 20px;
+
 }
 </style>
