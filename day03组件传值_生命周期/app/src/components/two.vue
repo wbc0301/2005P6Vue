97bb90e5ace11bbbf8002aba6d5d05fbca3e2310
wbc <417780894@qq.com>
2021-01-12 11:46:29 +0800

    父子传值

---
+++ day03组件传值_生命周期/app/src/components/two.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="two">
+		<h3>我是孩子组件</h3>
+		<!-- <div>{{ a}}{{ b}}</div> -->
+		<div>爸爸要送我一个礼物：{{a}}</div>
+		<button @click="song">aa</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['a', 'b'], // 接收父组件传递的数据
+	data() {
+		return {
+            str: '老师教你去学校'
+        };
+	},
+	methods: {
+		song() {
+            this.$emit('aaa', this.str) // 触发自定义事件
+		}
+	},
+};
+</script>
+
+<style lang='scss' scoped>
+.two {
+	height: 200px;
+	background-color: skyblue;
+	padding: 20px;
+}
+</style>
