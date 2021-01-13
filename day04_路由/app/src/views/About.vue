0eb50ce8fbd440bdfa34c287b59b200f50ad5b18
wbc <1548429568@qq.com>
2021-01-13 10:08:32 +0800

    router

--- day04_路由/app/src/views/About.vue
+++ day04_路由/app/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<button @click="toDetail">去详情页</button>
+	</div>
 </template>
+
+<script>
+export default {
+	methods: {
+		toDetail() {
+			// 通过编程式导航的方式实现路由跳转
+			this.$router.push('/detail')
+		}
+	}
+}
+</script>
